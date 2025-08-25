7b15363d116487d78005da5639ebb5b018e025a0
Humphrey47 <humphreymbise68@gmail.com>
2025-08-26 02:50:30 +0300

    Update Menu.js

--- plugins/Menu.js
+++ plugins/Menu.js
@@ -52,8 +52,8 @@ async (conn, mek, m, { from, quoted, body, isCmd, command, args, q, isGroup, sen
 ┃◈┃• ❣️ profile
 ┃◈┃• ❣️ antilink
 ┃◈┃• ❣️ game
-┃◈┃• ❣️ kiss
-┃◈┃• ❣️ general
+┃◈┃• ❣️ pdf
+┃◈┃• ❣️ hack
 ┃◈┃• ❣️ faith
 ┃◈┃• ❣️ zeze
 ┃◈╰─────────────────┈⊷
@@ -63,7 +63,7 @@ async (conn, mek, m, { from, quoted, body, isCmd, command, args, q, isGroup, sen
         await conn.sendMessage(
             from,
             {
-                image: { url: config.MENU_IMAGE_URL || 'https://files.catbox.moe/q8hps6.jpg' },
+                img { url: config.MENU_IMAGE_URL || 'https://files.catbox.moe/q8hps6.jpg' },
                 caption: dec,
                 contextInfo: {
                     mentionedJid: [m.sender],
