31785ad01788842718263b4fb11add7ccd0ede65
Humphrey47 <humphreymbise68@gmail.com>
2025-08-26 02:48:23 +0300

    Update Menu.js

--- plugins/Menu.js
+++ plugins/Menu.js
@@ -53,7 +53,7 @@ async (conn, mek, m, { from, quoted, body, isCmd, command, args, q, isGroup, sen
 ┃◈┃• ❣️ antilink
 ┃◈┃• ❣️ game
 ┃◈┃• ❣️ kiss
-┃◈┃• ❣️ love
+┃◈┃• ❣️ general
 ┃◈┃• ❣️ faith
 ┃◈┃• ❣️ zeze
 ┃◈╰─────────────────┈⊷
